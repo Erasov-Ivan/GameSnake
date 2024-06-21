0564f4d1a5f75ff11930a75994d37ae6cfff20dc
Erasov-Ivan <124393496+Erasov-Ivan@users.noreply.github.com>
2024-06-21 18:50:04 +0500

    Update food.c

--- Snake/food.c
+++ Snake/food.c
@@ -75,7 +75,31 @@ void CreateFood(Result* result, Snake* snake) {
 		result->error_code = 1;
 		return;
 	}
-	food->texture = LoadTexture("strawberry.png");
+	Texture* tmp = (Texture*)malloc(16 * sizeof(Texture));
+	if (!tmp) {
+		result->error = true;
+		result->error_code = 1;
+		return;
+	}
+	food->texture = tmp;
+	food->texture[0] = LoadTexture("strawberry/1.png");
+	food->texture[1] = LoadTexture("strawberry/2.png");
+	food->texture[2] = LoadTexture("strawberry/3.png");
+	food->texture[3] = LoadTexture("strawberry/4.png");
+	food->texture[4] = LoadTexture("strawberry/5.png");
+	food->texture[5] = LoadTexture("strawberry/6.png");
+	food->texture[6] = LoadTexture("strawberry/7.png");
+	food->texture[7] = LoadTexture("strawberry/8.png");
+	food->texture[8] = LoadTexture("strawberry/9.png");
+	food->texture[9] = LoadTexture("strawberry/10.png");
+	food->texture[10] = LoadTexture("strawberry/11.png");
+	food->texture[11] = LoadTexture("strawberry/12.png");
+	food->texture[12] = LoadTexture("strawberry/13.png");
+	food->texture[13] = LoadTexture("strawberry/14.png");
+	food->texture[14] = LoadTexture("strawberry/15.png");
+	food->texture[15] = LoadTexture("strawberry/16.png");
+
+	food->frame = 0;
 	GenerateFoodPosition(result, snake, food);
 	result->result = food;
 }
@@ -90,7 +114,9 @@ void DrawFood(Result* result, Food* food) {
 		result->error_code = 0;
 		return;
 	}
-	DrawTextureV(food->texture, food->position, WHITE);
+	DrawTextureV(food->texture[div(food->frame, 5).quot], food->position, WHITE);
+	food->frame++;
+	if (div(food->frame, 5).quot > 15) { food->frame = 0; }
 	result->error = false;
 }
 
@@ -104,6 +130,12 @@ void DeleteFood(Result* result, Food* food) {
 		result->error_code = 0;
 		return;
 	}
+	if (!food->texture) {
+		result->error = true;
+		result->error_code = 0;
+		return;
+	}
+	free(food->texture);
 	free(food);
 	result->error = false;
 }
